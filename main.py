8749e1d8d5678f96d2ca1eb45b950ced8f814554
SimonTheCoder <shihangyu@gmail.com>
2023-02-13 12:05:35 +0800

    multiple entries supported.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import openai
 import json
 import requests
@@ -15,7 +17,7 @@ api_key = config['DEFAULT']['api_key']
 openai.api_key = api_key
 
 # Define the text to be analyzed
-prompt = """请理解下面给出的信息，并给出各条内容的title、URL和summary。结果以json显示，key用英文，value尽量使用中文。信息："""
+prompt = """请理解下面给出的信息，并给出各条内容的title、url和summary。结果以json显示，key用英文，value尽量使用中文, 多个结果的情况下以array存放。信息："""
 
 # test_text = """• oss-security - double-free vulnerability in OpenSSH server 9.1:
 # https://www.openwall.com/lists/oss-security/2023/02/02/2
@@ -52,7 +54,7 @@ def on_button_click():
     response = openai.Completion.create(
               model="text-davinci-003",
               prompt=prompt+contents,
-              max_tokens=500,
+              max_tokens=2000,
               temperature=0
             )
 
@@ -62,65 +64,67 @@ def on_button_click():
     result_json = response["choices"][0]["text"]
 
 
-    with open("temp.txt","w") as f:
-        f.write(result_json)
+    with open("temp.txt","wb") as f:
+        f.write(result_json.encode("utf-8"))
 
-    # with open("temp.txt","r") as f:
+    # with open("temp.txt","rb") as f:
     #     result_json = f.read()
 
-    result = json.loads(result_json)
-    print(result)
+    result_list = json.loads(result_json)
 
-    #notion row
-    today = datetime.datetime.today()
-    date_str = today.strftime("%Y-%m-%d")
-    new_row = {
-        "Title": {"title": [{"text": {"content": result["title"]}}]},
-        "URL": {"url": result["URL"]},
-        "Summary":{"rich_text": [{"text": {"content": result["summary"]}}]},
-        "Found_date": {'id': 'uj%7Dj', 'type': 'date', 'date': {'start': date_str}},
-        "Tags": {'multi_select': [{ 'name': 'TAG_ME'}]}, 
+    for result in result_list:
+        print(result)
 
-    }
+        #notion row
+        today = datetime.datetime.today()
+        date_str = today.strftime("%Y-%m-%d")
+        new_row = {
+            "Title": {"title": [{"text": {"content": result["title"]}}]},
+            "URL": {"url": result["url"]},
+            "Summary":{"rich_text": [{"text": {"content": result["summary"]}}]},
+            "Found_date": {'id': 'uj%7Dj', 'type': 'date', 'date': {'start': date_str}},
+            "Tags": {'multi_select': [{ 'name': 'TAG_ME'}]}, 
 
-    row_page_body = [
-        {
-			"object": "block",
-			"type": "paragraph",
-			"paragraph": {
-				"rich_text": [
-					{
-						"type": "text",
-						"text": {
-							"content": contents,
-						}
-					}
-				]
-			}
         }
 
-    ]
-    #print(json.dumps(new_row))
-    label.config(text="Calling notion...")
-    response = requests.post(
-    "https://api.notion.com/v1/pages",
-    headers={"Authorization": f"Bearer {notion_api_key}",'Notion-Version': '2022-06-28'},
-    json={"parent": { "database_id": database_id },
-        "properties": new_row,
-        "children":row_page_body,
-        }
-    )
+        row_page_body = [
+            {
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": contents,
+                            }
+                        }
+                    ]
+                }
+            }
 
-    # Check the response status code to see if the request was successful
-    print(f"response is {response.status_code}")
-    if response.status_code == 200:
-        print("Row inserted successfully")
-        label.config(text="OK. Enter new content:")
-        #text.delete("1.0", tk.END)
-    else:
-        print("Failed to insert row")
-        print(f"response is {response.text}")
-        label.config(text=f"Failed: {response.text}")
+        ]
+        #print(json.dumps(new_row))
+        label.config(text="Calling notion...")
+        response = requests.post(
+        "https://api.notion.com/v1/pages",
+        headers={"Authorization": f"Bearer {notion_api_key}",'Notion-Version': '2022-06-28'},
+        json={"parent": { "database_id": database_id },
+            "properties": new_row,
+            "children":row_page_body,
+            }
+        )
+
+        # Check the response status code to see if the request was successful
+        print(f"response is {response.status_code}")
+        if response.status_code == 200:
+            print("Row inserted successfully")
+            label.config(text="OK. Enter new content:")
+            #text.delete("1.0", tk.END)
+        else:
+            print("Failed to insert row")
+            print(f"response is {response.text}")
+            label.config(text=f"Failed: {response.text}")
 
 button = tk.Button(root, text="Submit", command=on_button_click)
 button.pack()
